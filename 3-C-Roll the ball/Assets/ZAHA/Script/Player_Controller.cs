bf203605fe8666954216056bdc419fac55b703a6
ZAHA61 <65996616+ZAHA61@users.noreply.github.com>
2021-10-13 20:54:30 +0900

    player

--- 3-C-Roll the ball/Assets/ZAHA/Script/Player_Controller.cs
+++ 3-C-Roll the ball/Assets/ZAHA/Script/Player_Controller.cs
@@ -4,19 +4,4 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    private float Horizontal;
-    private float Vertical;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Horizontal = Input.GetAxis("Horizontal");
-        Vertical = Input.GetAxis("Vertical");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(Horizontal);
-        Debug.Log(Vertical);
-    }
 }
