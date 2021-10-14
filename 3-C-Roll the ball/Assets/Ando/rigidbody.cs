a83f1ad1218aa64b13a50d92a4fdc8fbdfca2e4f
安藤翼 <tubasa-ando-19h@stu.kbc.ac.jp>
2021-10-14 11:21:17 +0900

    少しいじった

---
+++ 3-C-Roll the ball/Assets/Ando/rigidbody.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidbody : MonoBehaviour
+{
+    Rigidbody rd;
+
+    void Start()
+    {
+        rd = this.GetComponent<Rigidbody>();
+        rd.useGravity = true;
+    }
+}
